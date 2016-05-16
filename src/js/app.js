19d00f7852a3ad3f793e9ef8bd7c7524de0c6982
Oleg Slobodskoi <oleg008@gmail.com>
2016-05-16 21:17:26 +0200

    university

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,75 @@
-(function extension() {
-  var installEl = document.querySelectorAll('.install-extension')[0]
+(function() {
 
-  installEl.addEventListener('click', function(e) {
+/* Utils */
+
+function toArray(obj) {
+  return [].slice.call(obj)
+}
+
+function $(selector, context) {
+  return toArray(document.querySelectorAll(selector, context))
+}
+
+/* Bindings */
+function ExtensionInstallation() {
+ function onInstall(e) {
     e.preventDefault()
 
-    if (!window.chrome) {
-      alert('Extension can be installed in Chrome browser only.')
+    if (!window.chrome || !window.chrome.webstore) {
+      alert('Extension can be installed in Desktop Chrome browser only.')
       return
     }
 
     chrome.webstore.install(
       'https://chrome.google.com/webstore/detail/dgeiagkojabjccafojhokcceakmehpbb',
       function() {
-        console.log('success')
+        location.hash = 'university/connect'
       },
       function(err) {
-        console.log('err', err)
+        alert(err)
       }
     )
-  })
-}())
+  }
 
+  $('.install-extension').forEach(function(el) {
+    el.addEventListener('click', onInstall)
+  })
+}
+
+function UniversityNav() {
+  var items = $('.university-nav a')
+  var scroller = $('.scroller')[0]
+  var selected
+
+  function select(item, name) {
+    var el = $('.' + name)[0]
+    // Vertical scroll.
+    smoothScroll(el, 500)
+    // Horizontal scroll.
+    smoothScroll(el, 500, function() {
+      item.classList.add('selected')
+      if (selected) selected.classList.remove('selected')
+      selected = item
+    }, scroller, 'horizontal')
+  }
+
+  function handleHashChange() {
+    if (!location.hash) return
+    var parts = location.hash.split('/')
+    var name = parts[1]
+    var item = items.filter(function(item) {
+      return item.href.indexOf(name) !== -1
+    })[0]
+    select(item, name)
+  }
+
+  window.addEventListener('hashchange', handleHashChange)
+  handleHashChange()
+}
+
+document.addEventListener('readystatechange', function() {
+  ExtensionInstallation()
+  UniversityNav()
+})
+
+}())
